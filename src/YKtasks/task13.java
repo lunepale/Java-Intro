cffbf85526e5484265e96fc8d53fd0440efa32f0
Vlad <49832690+lunepale@users.noreply.github.com>
2023-06-12 10:19:51 +0300

    YK tasks

--- src/YKtasks/task13.java
+++ src/YKtasks/task13.java
@@ -1,4 +1,7 @@
 package YKtasks;
+
+import java.util.Scanner;
+
 /*
 Напишите программу в которой:
 1) Пользователь должен ввести product name, price и qty для 5 продуктов
@@ -12,6 +15,57 @@ Note3: класс откуда будет раниться тест, долже�
  */
 public class task13 {
     public static void main(String[] args) {
+        String[] productName = new String[5];
+        double[] productPrice = new double[5];
+        int[] productQuantity = new int[5];
 
+      /*  for (int i = 0; i < 5; i++) {
+
+            System.out.println("Enter product name: ");
+            productName[i] = new Scanner(System.in).nextLine();
+            System.out.println("Enter product price: ");
+            productPrice[i] = new Scanner(System.in).nextDouble();
+            System.out.println("Enter product quantity: ");
+            productQuantity[i] = new Scanner(System.in).nextInt();
+        }*/
+
+        readInput(productName, productQuantity, productPrice);
+        calculate(productQuantity, productPrice, productName);
     }
+
+public static void readInput(String[] productName, int[] productQuantity, double[] productPrice) {
+Scanner scanner = new Scanner(System.in);
+
+    for (int i = 0; i < 5; i++){
+        System.out.println("Enter product name: ");
+    productName[i] = new Scanner(System.in).nextLine();
+    System.out.println("Enter product price: ");
+    productPrice[i] = new Scanner(System.in).nextDouble();
+    System.out.println("Enter product quantity: ");
+    productQuantity[i] = new Scanner(System.in).nextInt();}
 }
+
+
+
+
+        public static void calculate (int[] productQuantity, double[] productPrice, String[] productName) {
+        for (int i = 0; i < 5; i++) {
+
+
+            if (productPrice[i] <= 0 || productQuantity[i] <= 0) {
+
+                System.out.println("Price/quantity cannot be lower 0");
+                return;
+            }
+
+              double total = productQuantity[i] * productPrice[i];
+              System.out.println(productName[i] +" | "+total);
+          }}
+        }
+
+
+
+
+
+
+
